c2a625a90edad2677de253c1c88e2e039467c0fc
bhandariamey <bhandariamey@gmail.com>
2023-11-04 15:06:57 +0530

    My first React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+export default class Counter extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            count : 0
+        }
+    }
+
+    increaseCount = ()=>{
+        // setState -> its a callback.
+        this.setState((state,props)=>{
+            return {count: state.count + props.diff }
+        })
+    }
+
+    decreaseCount = ()=>{
+        this.setState((state,props)=>{
+            return {count: state.count - props.diff }
+        })
+    }
+
+    render(){
+        console.log(this.props)
+        return (
+        <div>
+            <h1 className="counter-display">{this.state.count}</h1>
+            <button onClick = {this.increaseCount}>+{this.props.diff}</button>
+            <button onClick={this.decreaseCount}>-{this.props.diff}</button>
+        </div> 
+        )
+        
+    }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react" // Component is derived from here
+import ReactDOM from "react-dom" // rendering is done using this.
+import Counter from "./App.js"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // const h1 = React.createElement("h1", null, "Hello World")
+// // console.log(h1)
+
+// // const element = React.createElement(<type-of-element>, <props>, <value>)
+// //const counterDisplayElement = React.createElement("h1",{className:"counter-display"},0);
+
+
+// // const incrementButtonElement = React.createElement("button",null,"+");
+// // const decrementButtonElement = React.createElement("button",null,"-");
+// // const containerDivElement = React.createElement("div",null,[counterDisplayElementJSX,incrementButtonElement,decrementButtonElement]);
+
+
+// const containerDivElementJSX = (
+//     <div>
+//       <h1 className="counter-display">0</h1>
+//       <button>+</button>
+//       <button>-</button>
+//     </div>
+//   )
+
+// ReactDOM.render(containerDivElementJSX, document.getElementById("root"))
+
+
+class App extends React.Component{
+    render(){
+        return (
+            <>
+            <Counter diff={1}/>
+            <Counter diff={10}/>
+            </>
+        )
+    }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App/>,rootElement)
